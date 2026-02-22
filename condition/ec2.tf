35e5ca0c6d3bb587cd88589ea521c57a9365944c
Jaya-Nagendra <nagendra.44@outlook.com>
2026-02-22 22:29:07 +0530

    terraform

---
+++ condition/ec2.tf
@@ -0,0 +1,32 @@
+resource "aws_instance" "variables_demo" {
+  ami           = var.ami_id
+  instance_type = var.environment == "dev" ? "t3.micro" : "t2.small"
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+
+  tags = var.ec2_tags
+}
+
+resource "aws_security_group" "allow_tls" {
+  name        = "variables_demo"
+  description = "Allow all inbound traffic and all outbound traffic"
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "variables_demo"
+  }
+}
---
+++ variables/variables.tf
@@ -0,0 +1,19 @@
+variable "ami_id" {
+  type = string   # this was not mandatory but we are guding to give 
+  default = "ami-0220d79f3f480ecf5"
+}
+
+variable "instance_size" {
+  type = string
+  default = "t3.micro"
+}
+
+variable "ec2_tags" {
+  type = map
+  default = {
+    Name = "Variables-demo"
+    Project = "roboshop"
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
